a311f36fd79e058c058746f41ef686256959079a
SaiC <sjc65@njit.edu>
2023-07-26 19:08:08 -0400

    added data link page to nav and updated data link search feature

--- partials/nav.php
+++ partials/nav.php
@@ -32,6 +32,7 @@ session_start();
         <?php if (is_logged_in()) : ?>
             <li><a href="<?php echo get_url('home.php'); ?>">Home</a></li>
             <li><a href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
+            <li><a href="<?php echo get_url('data_list.php'); ?>">Quotes List</a></li>
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
             <li><a href="<?php echo get_url('login.php'); ?>">Login</a></li>
--- public_html/Project/data_list.php
+++ public_html/Project/data_list.php
@@ -9,19 +9,20 @@ function getQuotesData($quoteSearchTerm = null, $authorSearchTerm = null, $limit
 {
     // Validate the limit value to be within the range (1 to 100)
     $limit = max(1, min(100, (int)$limit));
-
     $db = getDB();
 
     try {
         // Build the SQL query
-        $query = "SELECT quotes, author, API_Gen, created FROM Quotes WHERE 1";
+        $query = "SELECT quotes, author, API_Gen FROM Quotes WHERE 1";
 
         // Add search conditions if search terms are provided
         if ($quoteSearchTerm !== null) {
+            // Use the LIKE keyword for partial string matching
             $query .= " AND quotes LIKE :quoteSearchTerm";
         }
 
         if ($authorSearchTerm !== null) {
+            // Use the LIKE keyword for partial string matching
             $query .= " AND author LIKE :authorSearchTerm";
         }
 
@@ -56,7 +57,7 @@ if (isset($_GET['quoteSearch']) || isset($_GET['authorSearch']) || isset($_GET['
     // Get the search terms and limit from the user input
     $quoteSearchTerm = trim($_GET['quoteSearch']);
     $authorSearchTerm = trim($_GET['authorSearch']);
-    $limit = isset($_GET['limit']) ? (int)$_GET['limit'] : 10;
+    $limit = isset($_GET['limit']) ? (int)$_GET['limit'] : null; // Set limit to null by default
 
     // Call the function with the search terms and limit to get filtered data
     $quotesData = getQuotesData($quoteSearchTerm, $authorSearchTerm, $limit);
@@ -66,103 +67,196 @@ if (isset($_GET['quoteSearch']) || isset($_GET['authorSearch']) || isset($_GET['
         flash("No matches found");
     }
 } else {
-    // Call the function without search terms to get default data
-    $quotesData = getQuotesData();
+    // Get the total number of records in the database
+    $db = getDB();
+    $stmt = $db->query("SELECT COUNT(*) AS total_records FROM Quotes");
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    $totalRecords = (int)$result['total_records'];
+
+    // Set the default limit to the total number of records
+    $defaultLimit = $totalRecords;
+
+    // Call the function with the default limit to get default data
+    $quotesData = getQuotesData(null, null, $defaultLimit);
 }
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Quotes List</title>
     <!---------------------------Add CSS styling here -------------------------->
     <style>
-    /* CSS styling for the table container */
+        /* CSS styling for the table container */
         .table-container {
-            width: 1600px; /* Set the width to 150 pixels */
-            height: 500px; /* Set the height to 100 pixels */
-            overflow-y: scroll; /* Add vertical scrollbar */
-            border: 2px solid black; /* Black border for the container */
-            margin: 0 auto; /* Center the container on the page */
+            width: 1600px;
+            /* Set the width to 150 pixels */
+            height: 500px;
+            /* Set the height to 100 pixels */
+            overflow-y: scroll;
+            /* Add vertical scrollbar */
+            border: 2px solid black;
+            /* Black border for the container */
+            margin: 0 auto;
+            /* Center the container on the page */
         }
 
         /* CSS styling for the table */
         table {
             border-collapse: collapse;
-            width: 100%; /* Set the table width to 100% of the container */
+            width: 100%;
+            /* Set the table width to 100% of the container */
         }
 
-        th, td {
+        th,
+        td {
             padding: 10px;
-            border: 2px solid black; /* Black lines separating each cell */
-            text-align: center; /* Center the list items under each column */
+            border: 2px solid black;
+            /* Black lines separating each cell */
+            text-align: center;
+            /* Center the list items under each column */
         }
 
         th {
-            background-color: rgb(81, 81, 81); /* Black background for table headers */
-            color: white; /* White font color for table headers */
+            background-color: rgb(81, 81, 81);
+            /* Black background for table headers */
+            color: white;
+            /* White font color for table headers */
             font-family: "Lucida Console", "Courier New", monospace;
-            width: 80px; /* Shrink the "API?" column width to 80 pixels */
+            width: 80px;
+            /* Shrink the "API?" column width to 80 pixels */
         }
 
         /* Set column sizes */
         th:first-child,
         td:first-child {
-            width: 20px; /* "API?" column size (limited to 3 characters width) */
-            max-width: 3ch; /* Limit the column to 3 characters width */
-            white-space: nowrap; /* Prevent the text from wrapping */
-            overflow: hidden; /* Hide any overflow */
-            text-overflow: ellipsis; /* Show ellipsis for truncated text */
+            width: 20px;
+            /* "API?" column size (limited to 3 characters width) */
+            max-width: 3ch;
+            /* Limit the column to 3 characters width */
+            white-space: nowrap;
+            /* Prevent the text from wrapping */
+            overflow: hidden;
+            /* Hide any overflow */
+            text-overflow: ellipsis;
+            /* Show ellipsis for truncated text */
         }
 
         th:nth-child(2),
         td:nth-child(2) {
-            width: 300px; /* "Quotes" column size (100px wider) */
+            width: 300px;
+            /* "Quotes" column size (100px wider) */
         }
 
         th:nth-child(3),
         td:nth-child(3),
         th:nth-child(4),
         td:nth-child(4) {
-            width: 80px; /* "Author" and "Created" column size (remains the same) */
+            width: 80px;
+            /* "Author" and "Created" column size (remains the same) */
         }
 
         tr:nth-child(even) {
-            background-color: #f2f2f2; /* Gray background for even rows */
+            background-color: #f2f2f2;
+            /* Gray background for even rows */
         }
 
         .api-cell {
-        font-size: 20px; /* Increase font size to 16px */
+            font-size: 20px;
+            /* Increase font size to 16px */
         }
 
         .checkmark {
-            font-weight: bold; /* Make the checkmark bolder */
+            font-weight: bold;
+            /* Make the checkmark bolder */
+        }
+
+        /* --------------------------------------------------------------------------------------------- */
+        button {
+            background-color: rgb(81, 81, 81);
+            color: rgb(208, 199, 199);
+            margin-top: 10px;
+            margin: 10px 10px 0 0;
+        }
+
+        button:hover {
+            background-color: rgb(30, 31, 31);
+        }
+
+        .buttons-container {
+            margin-top: 10px;
+            text-align: center;
+        }
+
+        .buttons-container button {
+            padding: 6px;
+            margin: 5px 30px;
+            /* Add 30px separation between buttons */
+        }
+
+        /* --------------------------------------------------------------------------------------------- */
+        .search-bars {
+            display: flex;
+            align-items: center;
+            flex-wrap: wrap;
+        }
+
+        label {
+            margin-right: 10px;
+        }
+
+        input[type="text"],
+        input[type="number"] {
+            padding: 5px;
+            width: 200px;
+            border: 1px solid #000;
+        }
+
+        input[type="number"] {
+            width: 100px;
+        }
+
+
+        .separator {
+            margin: 0 10px;
+            border: none;
+            /* Remove the border */
+            height: 33px;
+            width: 2px;
+            background-color: #000;
         }
     </style>
     <!-------------------------------------------------------------------------->
-    </head>
+    
     <body>
-    <h1>Quotes List</h1>
-    <h2>Filter Quotes</h2>
+    <h2>Quotes List</h2>
     <form method="get" action="">
-        <label for="quoteSearch">Quote Search:</label>
-        <input type="text" name="quoteSearch" id="quoteSearch" placeholder="Enter search term">
-        <label for="authorSearch">Author Search:</label>
-        <input type="text" name="authorSearch" id="authorSearch" placeholder="Enter author name">
-        <label for="limit">Records Limit (1-100):</label>
-        <input type="number" name="limit" id="limit" min="1" max="100" value="10">
-        <button type="submit">Search</button>
+        <div class="search-bars">
+            <label for="quoteSearch">Quote Search:</label>
+            <input type="text" name="quoteSearch" id="quoteSearch" placeholder="Enter search term">
+            <div class="separator"></div> <!-- A small black line separator -->
+            <label for="authorSearch">Author Search:</label>
+            <input type="text" name="authorSearch" id="authorSearch" placeholder="Enter author name">
+            <div class="separator"></div> <!-- A small black line separator -->
+            <label for="limit">Records Limit (1-100):</label>
+            <input type="number" name="limit" id="limit" min="1" max="<?php echo $totalRecords; ?>" 
+                        value="<?php echo isset($limit) ? $limit : $defaultLimit; ?>">
+        </div>
+        <div class="buttons-container">
+            <button type="submit">Search</button>
+            <button type="button" onclick="window.location.href = 'data_list.php';">Refresh List</button>
+        </div>
     </form>
 
     <div class="table-container">
         <table>
             <tr>
-                <th>API?</th> 
+                <th>API?</th>
                 <th>Quotes</th>
                 <th>Author</th>
-                <th>Created</th>
             </tr>
-            <?php foreach ($quotesData as $quoteData): ?>
+            <?php foreach ($quotesData as $quoteData) : ?>
                 <tr>
                     <td>
                         <?php
@@ -175,15 +269,15 @@ if (isset($_GET['quoteSearch']) || isset($_GET['authorSearch']) || isset($_GET['
                             echo '<span class="checkmark">&#x2713;</span>';
                         }
                         ?>
-                    </td> 
+                    </td>
                     <td><?php echo htmlspecialchars($quoteData['quotes']); ?></td>
                     <td><?php echo htmlspecialchars($quoteData['author']); ?></td>
-                    <td><?php echo htmlspecialchars($quoteData['created']); ?></td>
                 </tr>
             <?php endforeach; ?>
         </table>
     </div>
 </body>
+
 </html>
 <?php
 require_once(__DIR__ . "/../../partials/flash.php");
