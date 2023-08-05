7dedaaa82571f215830fd5296cd9410da9d27a3b
SaiC <sjc65@njit.edu>
2023-08-05 19:02:03 -0400

    fixed bugs and completed basic features

--- public_html/Project/delete_favorites.php
+++ public_html/Project/delete_favorites.php
@@ -2,26 +2,36 @@
 require(__DIR__ . "/../../partials/nav.php");
 
 if (is_logged_in(true)) {
-    //comment this out if you don't want to see the session variables
+    // Comment this out if you don't want to see the session variables
     error_log("Session data: " . var_export($_SESSION, true));
 }
 ?>
+
 <?php
-if (!isset($_GET['record_id']) || !ctype_digit($_GET['record_id'])) {
-    flash("Invalid request. Record ID not specified", "danger");
+if (!isset($_GET['saved_id']) || !ctype_digit($_GET['saved_id'])) {
+    flash("Invalid request. Saved ID not specified", "danger");
     header("Location: " . get_url("favorites.php"));
     exit();
 }
 
-$recordId = (int)$_GET['record_id'];
+$savedId = (int)$_GET['saved_id'];
 
+// Retrieve data from the Saved_Quotes table
 $db = getDB();
+try {
+    $stmt = $db->prepare("SELECT sq.id AS saved_id, sq.user_id, sq.quote_id, q.quotes, q.author
+                          FROM Saved_Quotes sq
+                          INNER JOIN Quotes q ON sq.quote_id = q.id
+                          WHERE sq.id = :savedId");
+    $stmt->bindParam(":savedId", $savedId, PDO::PARAM_INT);
+    $stmt->execute();
+    $quoteData = $stmt->fetch(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    flash("An error occurred while retrieving quote data", "danger");
+}
 
 // Handle the deletion process
-
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
-    $savedId = (int)$_GET['id'];
-
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $db = getDB();
     try {
         $stmt = $db->prepare("DELETE FROM Saved_Quotes WHERE id = :savedId");
@@ -32,10 +42,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
     } catch (PDOException $e) {
         flash("An error occurred while deleting the quote record", "danger");
     }
-}
 
-header("Location: " . get_url("favorites.php"));
-exit();
+    header("Location: " . get_url("favorites.php"));
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
@@ -47,19 +57,25 @@ exit();
 
 <body>
     <h2>Delete Record?</h2>
+    <h3>Are you sure you want to delete this record from favorites?</h3>
 
-    <?php if ($recordId !== null) : ?>
-        <p>Are you sure you want to delete this record?</p>
-
+    <?php if ($savedId !== null) : ?>
         <!-- Form to confirm the deletion -->
         <form method="post">
+            <!-- Hidden input to send the saved_id to the script -->
+            <input type="hidden" name="saved_id" value="<?php echo $savedId; ?>">
             <!-- Button to confirm deletion -->
             <button type="submit">Delete</button>
         </form>
+        <!-- Button to redirect back to favorites page -->
+        <a href="<?php echo get_url('favorites.php'); ?>">
+            <button type="button">Cancel</button>
+        </a>
     <?php endif; ?>
 </body>
 
 </html>
+
 <?php
 require(__DIR__ . "/../../partials/flash.php");
 ?>
--- public_html/Project/favorites.php
+++ public_html/Project/favorites.php
@@ -1,20 +1,15 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
-
-if (is_logged_in(true)) {
-    //comment this out if you don't want to see the session variables
-    error_log("Session data: " . var_export($_SESSION, true));
-}
 ?>
 <?php
-function getSavedQuotesData($limit = 10)
+function getSavedQuotesData($user_id, $limit = 10)
 {
     $limit = max(1, min(100, (int)$limit));
     $db = getDB();
-    $user_id = 1;   // Defaulted at 1 for testing purposes. Change this to actual logged-in user ID
 
     try {
-        $query = "SELECT sq.id AS saved_id, sq.quote_id, sq.quotes, sq.author FROM Saved_Quotes sq
+        $query = "SELECT sq.id AS saved_id, sq.user_id, sq.quote_id, q.quotes, q.author FROM Saved_Quotes sq
+                  INNER JOIN Quotes q ON sq.quote_id = q.id
                   WHERE sq.user_id = :user_id LIMIT :limit";
 
         $stmt = $db->prepare($query);
@@ -24,24 +19,31 @@ function getSavedQuotesData($limit = 10)
         $savedQuotesData = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         return $savedQuotesData;
-
     } catch (PDOException $e) {
         flash("An error occurred while retrieving data from the database", "danger");
         return [];
     }
 }
-if (is_logged_in()) {
-    $user_id = get_user_id();   
-    $defaultLimit = 10;
 
-    if (isset($_GET['limit'])) {
-        $limit = max(1, min(100, (int)$_GET['limit']));
-    } else {
-        $limit = $defaultLimit;
-    }
+$user_id = get_user_id();
+$defaultLimit = 10;
 
-    // Call the function to get saved quotes data for the logged-in user
-    $savedQuotesData = getSavedQuotesData($user_id, $limit);
+if (isset($_GET['limit'])) {
+    $limit = max(1, min(100, (int)$_GET['limit']));
+} else {
+    $limit = $defaultLimit;
+}
+
+// Call the function to get saved quotes data for the logged-in user
+$savedQuotesData = getSavedQuotesData($user_id, $limit);
+
+// Loops through the retrieved data and assigns them to variables
+foreach ($savedQuotesData as $quoteData) {
+    $saved_id = $quoteData['saved_id'];
+    $user_id = $quoteData['user_id'];
+    $quote_id = $quoteData['quote_id'];
+    $quote = $quoteData['quotes'];
+    $author = $quoteData['author'];
 }
 ?>
 
@@ -51,7 +53,7 @@ if (is_logged_in()) {
 <head>
     <title>Saved Quotes List</title>
     <style>
-        
+
     </style>
 </head>
 
@@ -76,11 +78,10 @@ if (is_logged_in()) {
                     <td><?php echo htmlspecialchars($savedQuoteData['quotes']); ?></td>
                     <td><?php echo htmlspecialchars($savedQuoteData['author']); ?></td>
                     <td>
-                        <form method="post" action="delete_favorites.php">
-                            <a href="delete_favorites.php?id=<?php echo $savedQuoteData['saved_id']; ?>">
-                                <button type="submit">Delete</button>
-                            </a>
-                        </form>
+                        <!-- Button to redirect to this record's delete page -->
+                        <a href="delete_favorites.php?saved_id=<?php echo $savedQuoteData['saved_id']; ?>">
+                            <button>Delete</button>
+                        </a>
                     </td>
                 </tr>
             <?php endforeach; ?>
@@ -89,8 +90,6 @@ if (is_logged_in()) {
 </body>
 
 </html>
-
-
 <?php
 require(__DIR__ . "/../../partials/flash.php");
 ?>
