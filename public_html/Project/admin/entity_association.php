4799c57684478d22a45a2fa849a514f37b98a5be
SaiC <sjc65@njit.edu>
2023-08-07 00:09:56 -0400

    added admin entity association page baseline and updated nav

---
+++ public_html/Project/admin/entity_association.php
@@ -0,0 +1,18 @@
+<?php
+require(__DIR__ . "/../../../partials/nav.php");
+if (!has_role("Admin")) {
+    flash("You don't have permission to view this page", "warning");
+    die(header("Location: " . get_url("home.php")));
+}
+?>
+<?php
+
+
+?>
+
+
+
+
+<?php
+require_once(__DIR__ . "/../../../partials/flash.php");
+?>
